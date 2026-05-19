fad4a436ad6fd31ced51bde662a8d7c953e68ac2
chienngoc2 <chien01656822826@gmail.com>
2026-05-19 16:33:08 +0700

    first commit

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:5000";
-
+console.log("URL Backend hiện tại là:", process.env.EXPO_PUBLIC_API_URL);
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
