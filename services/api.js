3ee09d54fcb0d2f9a6d82c8b76328a2ec5aec620
chienngoc2 <chien01656822826@gmail.com>
2026-05-19 15:02:31 +0700

    update link

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// THAY CÁI NÀY BẰNG IP SẾP VỪA TÌM ĐƯỢC
-const BASE_URL = "http://192.168.132.1:5000";
+
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:5000";
 
 const api = axios.create({
   baseURL: BASE_URL,
